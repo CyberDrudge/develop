d8092e637626484911e5b57c7d1fc87d7b5d5cb5
Marc Pare <marcpare0@gmail.com>
2016-10-06 20:32:27 -0700

    Tweak password changing script for dev

--- scripts/local-restore-snapshot.sh
+++ scripts/local-restore-snapshot.sh
@@ -1,2 +1,2 @@
 sudo -u postgres pg_restore --verbose --clean --no-acl --no-owner -d sums $1
-sudo -u postgres psql -c "update users set password='password';"
+sudo -u postgres psql -d sums -c "update users set password='password';"
